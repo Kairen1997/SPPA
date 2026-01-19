422804370f4e58d78172491168c999909c0f6e98
Kairen1997 <113156980+Kairen1997@users.noreply.github.com>
2026-01-19 10:28:24 +0800

    Add activity and notification handling in PembangunanLive
    - Introduced logic to fetch recent activities and notifications count when connected.
    - Updated the HTML template to utilize a new header component for notifications and activities display.

--- lib/sppa_web/live/pembangunan_live.ex
+++ lib/sppa_web/live/pembangunan_live.ex
@@ -1,6 +1,8 @@
 defmodule SppaWeb.PembangunanLive do
   use SppaWeb, :live_view
 
+  alias Sppa.Projects
+
   @allowed_roles ["pembangun sistem", "pengurus projek", "ketua penolong pengarah"]
 
   @impl true
@@ -28,8 +30,20 @@ defmodule SppaWeb.PembangunanLive do
         |> assign(:show_edit_modal, false)
         |> assign(:selected_module, nil)
         |> assign(:form, to_form(%{}, as: :module))
+        |> assign(:activities, [])
+        |> assign(:notifications_count, 0)
 
-      {:ok, socket}
+      if connected?(socket) do
+        activities = Projects.list_recent_activities(socket.assigns.current_scope, 10)
+        notifications_count = length(activities)
+
+        {:ok,
+         socket
+         |> assign(:activities, activities)
+         |> assign(:notifications_count, notifications_count)}
+      else
+        {:ok, socket}
+      end
     else
       socket =
         socket
